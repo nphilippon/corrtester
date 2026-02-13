d2f2b90ba326d3b748f53ff2500fe7f09f6612eb
Her Name <hergithubemail@example.com>
2026-02-13 10:13:08 -0700

    gt add

--- requirements.R
+++ requirements.R
@@ -1,3 +1,3 @@
 #Cleanly install all the packages that were used in this shinyapp
-dockerpackages <- c("shiny", "shinythemes" , "tidyverse", "tidyquant", "plotly", "corrplot", "moments")
+dockerpackages <- c("shiny", "shinythemes" , "tidyverse", "tidyquant", "plotly", "corrplot", "moments", "gt")
 install.packages(dockerpackages, dependencies = TRUE)
